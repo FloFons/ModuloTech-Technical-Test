cff43abb4374e25e3c8881cf6b950bee1334cc1d
FloFons <florent.fonsalas@frichti.co>
2022-09-05 15:47:22 +0200

    init application

---
+++ ModuloTech/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  ModuloTech
+//
+//  Created by Florent Fonsalas on 05/09/2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
